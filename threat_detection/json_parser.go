0c93ad7323562249d52a08b31dae699e5b79ebc9
dev4fun007 <dev4fun007@gmail.com>
2019-09-05 00:34:43 +0530

    traversing json objects

--- threat_detection/json_parser.go
+++ threat_detection/json_parser.go
@@ -1,5 +1,38 @@
-package threat_detection
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+)
 
 func main() {
-	
+	const jsonStream = `{"Message": "Hello", "Array": [1, 2, 3], "Null": null, "Number": 1.234}`
+
+	decoder := json.NewDecoder(strings.NewReader(jsonStream))
+	for {
+		t, err := decoder.Token()
+		if err == io.EOF {
+			break
+		} else {
+			checkError(err)
+		}
+
+		tokenType := t.(json.Delim)
+		fmt.Print(tokenType)
+
+		fmt.Printf("%T : %v", t, t)
+		if decoder.More() {
+			fmt.Printf(" more ")
+		}
+		fmt.Printf("\n")
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
